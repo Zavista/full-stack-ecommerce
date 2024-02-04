a8b33b31e145f8eefb10e7e81b88fa95a08e012a
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 16:18:01 -0500

    Added mobile items section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-
+import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>Hi</div>
+    <div className="p-5">
+      <div>
+        <h2 className="text-2xl">Mobiles</h2>
+        <div className="py-4">
+          <div className="w-64">
+            <div className="bg-blue-100 p-5 rounded-xl">
+              <Image src='/products/iphone.png' alt="iphone" width={200} height={150}></Image>
+            </div>
+            <div className="mt-2">
+              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
+            </div>
+            <p className="text-sm mt-1 leading-4">Astonishing 6.5" display, powerful A16 chip, and a stellar triple-lens camera system. Face ID is faster, and 5G connectivity keeps you on the cutting edge. MagSafe adds convenience. In a nutshell, it's the epitome of smartphone brilliance.</p>
+            <div className="flex items-center justify-between mt-1">
+              <div className="text-2xl font-bold">$899</div>
+              <button className="bg-emerald-400 text-white py-1 px-3">Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
