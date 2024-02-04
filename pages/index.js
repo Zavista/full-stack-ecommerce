79f588131744a8c861ba68ef19c8a28bcb6eb56e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 18:47:53 -0500

    Map each product to a ProductCard

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
-
+import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
 
@@ -14,25 +14,21 @@ export default function Home() {
     getProducts();
   },[])
 
+  const categoriesNames  = [... new Set(productsInfo.map(product => product.category))];
+
   return (
     <div className="p-5">
       <div>
-        <h2 className="text-2xl">Mobiles</h2>
-        <div className="py-4">
-          <div className="w-64">
-            <div className="bg-blue-100 p-5 rounded-xl">
-              <Image src='/products/iphone.png' alt="iphone" width={200} height={150}></Image>
-            </div>
-            <div className="mt-2">
-              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
-            </div>
-            <p className="text-sm mt-1 leading-4">Astonishing 6.5" display, powerful A16 chip, and a stellar triple-lens camera system. Face ID is faster, and 5G connectivity keeps you on the cutting edge. MagSafe adds convenience. In a nutshell, it's the epitome of smartphone brilliance.</p>
-            <div className="flex items-center justify-between mt-1">
-              <div className="text-2xl font-bold">$899</div>
-              <button className="bg-emerald-400 text-white py-1 px-3">Add to Cart</button>
-            </div>
+        {categoriesNames.map(category => (
+          <div key={category}>
+            <h2 className="text-2xl capitalize">{category}</h2>
+            {productsInfo.filter(product => product.category === category).map(product => (
+              <div key={product.name}>
+                <ProductCard name={product.name} price={product.price} description={product.description} picture={product.picture}></ProductCard>
+              </div>
+            ))}
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
