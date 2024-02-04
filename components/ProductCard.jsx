42607f8fc7058a3376ca8321f26b703b5c9fe45f
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 18:56:37 -0500

    Fixed image sizing

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({name, price, description, picture}) => {
   return (
     <div className="w-64">
         <div className="bg-blue-100 p-5 rounded-xl">
-            <Image src={picture} alt="iphone" width={200} height={150}></Image>
+            <img src={picture} alt="iphone"  className='w-[250px] h-[200px] object-contain'></img>
         </div>
         <div className="mt-2">
             <h3 className="font-bold text-lg">{name}</h3>
