c37021a5666882199376cc45d58588ebcf99dd02
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 18:11:02 -0500

    Update handle to return product data

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,6 +1,8 @@
 import { connectMongoose } from "@/lib/mongoose";
+import Product from "@/models/Product";
 
 export default async function handle(req, res) {
     await connectMongoose();
-    
+    const response = await Product.find().exec();
+    res.status(200).json(response);
 }
