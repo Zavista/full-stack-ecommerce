faba6e067d033a1aec1366ed126b54d0454a9c7d
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 20:39:28 -0500

    Add function to updateSelected products when add to cart

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Image from 'next/image';
+import { ProductsContext } from './ProductsContext';
 
 const ProductCard = ({_id, name, price, description, picture}) => {
+  const {setSelectedProducts } = useContext(ProductsContext);
+  const addProduct = () => {
+    setSelectedProducts(prev => [...prev, _id]); //add _id to end of list
+  };
+
   return (
     <div className="w-64">
         <div className="bg-blue-100 p-5 rounded-xl">
@@ -13,7 +19,9 @@ const ProductCard = ({_id, name, price, description, picture}) => {
         <p className="text-sm mt-1 leading-4">{description}</p>
         <div className="flex items-center justify-between mt-1">
               <div className="text-2xl font-bold">{`$${price}`}</div>
-              <button className="bg-emerald-400 text-white py-1 px-3">Add to Cart</button>
+              <button 
+              onClick={() => addProduct()}
+              className="bg-emerald-400 text-white py-1 px-3">Add to Cart</button>
         </div>
     </div>
   )
