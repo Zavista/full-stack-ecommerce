006b468287ef48842df021366b481dc46f3653c0
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 21:23:51 -0500

    Reformatted handle

--- pages/api/products.js
+++ pages/api/products.js
@@ -7,16 +7,20 @@ export async function findAllProducts() {
 }
 
 export default async function handle(req, res) {
-  await connectMongoose();
-  const {ids} = req.query;
-  if (ids) {
-    const idsArray = ids.split(',');
-    res.json(
-      await Product.find({
-        '_id':{$in:idsArray}
-      }).exec()
-    );
-  } else {
-    res.json( await findAllProducts() );
+    await connectMongoose();
+    
+    const { ids } = req.query;
+  
+    if (ids) {
+      const idsArray = ids.split(',');
+  
+      const products = await Product.find({
+        '_id': { $in: idsArray }
+      }).exec();
+  
+      res.json(products);
+    } else {
+      const allProducts = await findAllProducts();
+      res.json(allProducts);
+    }
   }
-}
