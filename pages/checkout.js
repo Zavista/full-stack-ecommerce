d7ccd2938864b92c2f8c45cc1abf1de02155ee06
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 21:02:42 -0500

    Add logic to fetch data from API

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,9 +1,26 @@
 import Layout from "@/components/Layout"
+import { ProductsContext } from "@/components/ProductsContext"
+import { useContext, useEffect, useState } from "react"
 
 const checkout = () => {
+  const {selectedProducts} = useContext(ProductsContext);
+  const [productsInfo, setProductsInfo] = useState([]);
+  const getProducts = async () => {
+    const uniqIds = [...new Set(selectedProducts)];
+    const response = await fetch('/api/products?id='+uniqIds.join(', '));
+    const data = await response.json();
+    console.log(data);
+    setProductsInfo(data);
+    
+  }
+
+  useEffect(() => {
+    getProducts()
+  },[selectedProducts])
+
   return (
     <Layout>
-        checkout
+        {selectedProducts.join(', ')}
     </Layout>
   )
 }
