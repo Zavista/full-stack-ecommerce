a7c68b5a03a8ca23ed13e32da0d723440d1726e4
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 20:36:10 -0500

    Removed extra ;

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
   <ProductsContextProvider>
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </ProductsContextProvider>
   
   )
