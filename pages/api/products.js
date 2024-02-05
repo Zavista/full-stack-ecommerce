3d8a2051e4e70d33f76ab319b334c7eeae8d2edc
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 21:22:22 -0500

    Update API to handle find some products

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,12 +1,22 @@
 import { connectMongoose } from "@/lib/mongoose";
 import Product from "@/models/Product";
 
-export default async function handle(req, res) {
-    await connectMongoose();
-    const response = await Product.find().exec();
-    res.status(200).json(response);
-}
 
 export async function findAllProducts() {
-    return Product.find().exec();
+  return Product.find().exec();
+}
+
+export default async function handle(req, res) {
+  await connectMongoose();
+  const {ids} = req.query;
+  if (ids) {
+    const idsArray = ids.split(',');
+    res.json(
+      await Product.find({
+        '_id':{$in:idsArray}
+      }).exec()
+    );
+  } else {
+    res.json( await findAllProducts() );
+  }
 }
