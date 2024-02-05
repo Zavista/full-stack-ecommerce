453edad56687619cd3ebce4c43bda53fb2496c9e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-04 21:22:33 -0500

    fixed typo in fecth request

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,9 +5,10 @@ import { useContext, useEffect, useState } from "react"
 const checkout = () => {
   const {selectedProducts} = useContext(ProductsContext);
   const [productsInfo, setProductsInfo] = useState([]);
+
   const getProducts = async () => {
     const uniqIds = [...new Set(selectedProducts)];
-    const response = await fetch('/api/products?id='+uniqIds.join(', '));
+    const response = await fetch(`/api/products?ids=${uniqIds.join(',')}`);
     const data = await response.json();
     console.log(data);
     setProductsInfo(data);
