8e631fc2b64b00582d44ef3d53dd8ca5ca9c28eb
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-05 20:03:51 -0500

    Fixed spacing issue with price and description

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -9,20 +9,25 @@ const ProductCard = ({_id, name, price, description, picture}) => {
   };
 
   return (
-    <div className="w-64">
+    <div className="w-64 h-[100%] flex flex-col">
         <div className="bg-blue-100 p-5 rounded-xl">
             <img src={picture} alt="iphone"  className='w-[220px] h-[180px] md:w-[300px] md:h-[225px] object-contain'></img>
         </div>
-        <div className="mt-2">
-            <h3 className="font-bold text-lg">{name}</h3>
+        <div className='h-[100%] flex flex-col justify-between items-between'>
+          <div>
+              <div className="mt-2">
+                <h3 className="font-bold text-lg">{name}</h3>
+              </div>
+              <p className="text-sm mt-1 leading-4">{description}</p>
+          </div>
+          <div className="flex items-center justify-between mt-1">
+                <div className="text-2xl font-bold">{`$${price}`}</div>
+                <button 
+                onClick={() => addProduct()}
+                className="bg-emerald-400 text-white py-1 px-3">Add to Cart</button>
+          </div>          
         </div>
-        <p className="text-sm mt-1 leading-4">{description}</p>
-        <div className="flex items-center justify-between mt-1">
-              <div className="text-2xl font-bold">{`$${price}`}</div>
-              <button 
-              onClick={() => addProduct()}
-              className="bg-emerald-400 text-white py-1 px-3">Add to Cart</button>
-        </div>
+
     </div>
   )
 }
