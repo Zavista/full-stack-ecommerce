7f162d8387d8531ca0afed21795cd4bf3733ae8e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-05 19:11:21 -0500

    Added Stripe checkout

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,5 +1,6 @@
 import { connectMongoose } from "@/lib/mongoose";
-const stripe = require('stripe')('sk_test_...');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Product from "@/models/Product";
 
 export default async function handler(req, res) {
     await connectMongoose();
@@ -9,6 +10,31 @@ export default async function handler(req, res) {
         return;
     }
 
-    res.json
+    const productsIds = req.body.products.split(',');
+    const uniqIds = [...new Set(productsIds)];
+    const products = await Product.find({_id:{$in:uniqIds}});
+
+    let line_items = [];
+    for (let productId of uniqIds) {
+        const quantity = productsIds.filter(id => id === productId).length;
+        const product = products.find(p => p._id.toString() === productId);
+        line_items.push({
+            quantity: quantity,
+            price_data: {
+                currency: 'USD',
+                product_data: {name: product.name},
+                unit_amount: product.price * 100, //cause Stripe uses cents, so I need to turn $100 into 10,000 cents
+            },
+
+        })
+    }
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: line_items,
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.redirect(303, session.url);
 
 }
