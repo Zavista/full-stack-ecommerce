e7be64c2dd5b1fa2c9be6fd95d961b7adb724a29
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-05 19:43:43 -0500

    Update layout to display order success/fail

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
+import { useEffect } from 'react';
 
 const Layout = ({children}) => {
+  const [success, setSuccess] = useState("");
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      setSuccess('success');
+    }
+
+    if (query.get('canceled')) {
+      setSuccess('canceled');
+    }
+  }, []);
+
   return (
     <div>
-      <div className='p-5'>{children}</div> 
+      <div className='p-5'>
+      {success === 'success' && (
+        <div className="mb-5 bg-green-400 text-white text-lg p-5 rounded-xl">
+          Thanks for your order!
+        </div>
+        )
+      }
+      {success === 'canceled' && (
+        <div className="mb-5 bg-red-400 text-white text-lg p-5 rounded-xl">
+          Order Canceled!
+        </div>
+        )
+      } 
+        {children}</div> 
       <Footer></Footer>
     </div>
     
