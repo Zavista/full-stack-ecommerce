078951033e69402d8054081003dfe1e0e54b41ef
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-05 19:31:57 -0500

    Update Order model to store customer information

--- models/Order.js
+++ models/Order.js
@@ -4,6 +4,12 @@ import { models } from "mongoose";
 const OrderSchema = new mongoose.Schema(
     {
         products: Object,
+        name: String,
+        email: String,
+        address: String,
+        city: String,
+        country: String,
+        postal: String,
         paid: { type: Number, default: 0 },
         date: { type: Date, default: Date.now } // Set the default value as the current date
     }
